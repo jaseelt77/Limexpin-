8916baa3921cf0f4bc8ad705e4e3f84fae0170ad
jaseel <jaseel.t@mfine.co>
2026-01-12 21:15:29 +0530

    web files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // for app folder
+    "./pages/**/*.{js,ts,jsx,tsx}", // for pages folder
+    "./components/**/*.{js,ts,jsx,tsx}" // for components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
